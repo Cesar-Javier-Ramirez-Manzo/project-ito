190aa8f097a8b80fea0ddae5e6afe039060b212e
Cesar-Javier-Ramirez-Manzo <50338995+Cesar-Javier-Ramirez-Manzo@users.noreply.github.com>
2020-11-19 22:51:15 -0600

    Update project.js

--- project.js
+++ project.js
@@ -1,7 +1,7 @@
 var indiceBu=0
 var cont=0
 var subca;
-
+var acces=false;
 document.addEventListener('keyup', (evento)=>{
     var dinosaurio= document.getElementById('dino')
     var dinStyle=window.getComputedStyle(dinosaurio)
@@ -12,6 +12,7 @@ document.addEventListener('keyup', (evento)=>{
     if(texto==""){
         indiceBu=0
         subca=null
+        acces=false
     }
     
     
@@ -19,48 +20,57 @@ document.addEventListener('keyup', (evento)=>{
     subca=texto.substring(indiceBu)
     console.log(subca)
     var sujet=subca.search("vaca");
-    if(sujet!=-1){
-        var direccion=subca.search("hacia");
-
-        
-
-        if(direccion!=-1){
-            var arriba=subca.search("arriba");
-            if(arriba!=-1){
+    var sujet1=subca.search("tu");
+    if(sujet!=-1||sujet1!=-1){
+        acces=true
+    }
+    var neg=subca.search("no");
+    if(neg==-1){
+        if(acces==true){
+            var direccion=subca.search("hacia");
+            var direccion1=subca.search("a la");
+            var direccion2=subca.search("para");
             
-            var newDir=(parseInt(dinStyle.getPropertyValue('margin-top'))-25)+"px";
-            dinosaurio.style.marginTop= newDir 
-            indiceBu=indiceBu+arriba+6
-            cont++
-            }
-
-            var abajo=subca.search("abajo");
-            if(abajo!=-1){
-            
-                var newDir=(parseInt(dinStyle.getPropertyValue('margin-top'))+25)+"px";
+    
+            if(direccion!=-1||direccion1!=-1||direccion2!=-1){
+                var arriba=subca.search("arriba");
+                if(arriba!=-1){
+                
+                var newDir=(parseInt(dinStyle.getPropertyValue('margin-top'))-25)+"px";
                 dinosaurio.style.marginTop= newDir 
-                indiceBu=indiceBu+abajo+5
+                indiceBu=indiceBu+arriba+6
                 cont++
-            }
-
-            var dere=subca.search("derecha");
-            if(dere!=-1){
-            
-                var newDir=(parseInt(dinStyle.getPropertyValue('margin-left'))+25)+"px";
-                dinosaurio.style.marginLeft= newDir 
-                indiceBu=indiceBu+dere+7
-                cont++
-            }
-            var izquierda=subca.search("izquierda");
-            if(izquierda!=-1){
-            
-                var newDir=(parseInt(dinStyle.getPropertyValue('margin-left'))-25)+"px";
-                dinosaurio.style.marginLeft= newDir 
-                indiceBu=indiceBu+izquierda+9
-                cont++
+                }
+    
+                var abajo=subca.search("abajo");
+                if(abajo!=-1){
+                
+                    var newDir=(parseInt(dinStyle.getPropertyValue('margin-top'))+25)+"px";
+                    dinosaurio.style.marginTop= newDir 
+                    indiceBu=indiceBu+abajo+5
+                    cont++
+                }
+    
+                var dere=subca.search("derecha");
+                if(dere!=-1){
+                
+                    var newDir=(parseInt(dinStyle.getPropertyValue('margin-left'))+25)+"px";
+                    dinosaurio.style.marginLeft= newDir 
+                    indiceBu=indiceBu+dere+7
+                    cont++
+                }
+                var izquierda=subca.search("izquierda");
+                if(izquierda!=-1){
+                
+                    var newDir=(parseInt(dinStyle.getPropertyValue('margin-left'))-25)+"px";
+                    dinosaurio.style.marginLeft= newDir 
+                    indiceBu=indiceBu+izquierda+9
+                    cont++
+                }
             }
         }
     }
+    
     
     
 
